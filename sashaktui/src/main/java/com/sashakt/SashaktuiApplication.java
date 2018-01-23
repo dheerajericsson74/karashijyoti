e44f119b2e1944d88900420171c4e25f84eccaff
Dheeraj Sharma <dheeraj.sharma@ericsson.com>
2018-01-23 13:12:56 +0530

    git init

---
+++ sashaktui/src/main/java/com/sashakt/SashaktuiApplication.java
@@ -0,0 +1,12 @@
+package com.sashakt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SashaktuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SashaktuiApplication.class, args);
+	}
+}
---
+++ sashaktui/src/test/java/com/sashakt/SashaktuiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sashakt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SashaktuiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
